24e8221fd0ae20c2ea0369ff82e004072bbb2983
Sebastian Mendez <sebamndz@gmail.com>
2020-10-04 14:18:08 -0300

    guardar

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const naranja = document.getElementById('naranja');
 const verde = document.getElementById('verde');
 const btnEmpezar = document.getElementById('btnEmpezar');
 btnEmpezar.addEventListener("click", empezarJuego);
-const ULTIMO_NIVEL = 10
+const Nivel = document.getElementById("Nivel");
+const ULTIMO_NIVEL = 10;
 
 class Juego{
     constructor() {
@@ -14,9 +15,9 @@ class Juego{
 }
 
 inicializar() {
+    btnEmpezar.classList.add('hide')
     this.siguienteNivel = this.siguienteNivel.bind(this)
     this.elegirColor = this.elegirColor.bind(this)
-    btnEmpezar.classList.add('hide')
     this.nivel = 1
     this.colores = {
         celeste,
@@ -39,7 +40,7 @@ siguienteNivel() {
 transformarNumeroAColor(numero) {
     switch (numero) {
         case 0:
-            return "celeste";
+            return "celeste"
         case 1:
             return "violeta"
         case 2:
@@ -66,6 +67,7 @@ iluminarSecuencia() {
     for (let i = 0; i < this.nivel; i++) {
         const color = this.transformarNumeroAColor(this.secuencia[i])
         setTimeout(() => this.iluminarColor(color),1000 * i)
+    Nivel.innerHTML =` Nivel ${this.nivel}`
     }
 }
 
@@ -99,14 +101,21 @@ iluminarColor(color) {
             this.nivel++
             this.eliminarEventosClick()
             if (this.nivel === (ULTIMO_NIVEL + 1)) {
-              // Ganó!
+                this.ganoElJuego()
             } else {
                 setTimeout(this.siguienteNivel, 1500)
             }
-            }
-        } else {
-          // Perdió
+        }   
+            } else {
+                this.perdioElJuego()
         }
+    }
+    ganoElJuego() {
+        Nivel.innerHTML = `Ganaste! :O`
+    }
+    perdioElJuego() {
+        Nivel.innerHTML= `Perdiste :(`
+        this.eliminarEventosClick()
     }
 }
 
